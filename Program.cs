7ce3083e93c9f1bd1a2881b358be938e3dd04c80
hannestrieb <triebhannes@gmail.com>
2026-01-22 15:31:22 +0100

    added multiply and divide

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ app.MapGet("/add", (int num1, int num2) => AddNumbers(num1, num2));
 
 app.MapGet("/subtract", (int num1, int num2) => SubtractNumbers(num1, num2));
 
+app.MapGet("/multiply", (int num1, int num2) => MultiplyNumbers(num1, num2));
+
+app.MapGet("/divide", (int num1, int num2) => DivideNumbers(num1, num2));
+
 app.Run();
 
 string AddNumbers(int num1, int num2)
@@ -17,4 +21,14 @@ string AddNumbers(int num1, int num2)
 string SubtractNumbers(int num1, int num2)
 {
     return $"Differensen av {num1} och {num2} är: {num1 - num2}";
+}
+
+string MultiplyNumbers(int num1, int num2)
+{
+    return $"Produkten av {num1} och {num2} är {num1 * num2}";
+}
+
+string DivideNumbers(int num1, int num2)
+{
+    return $"Kvoten av {num1} och {num2} är {num1 / (double)num2}";
 }
