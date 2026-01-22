eb40d73bed0c1e49b99683940bfceec39066fde3
hannestrieb <triebhannes@gmail.com>
2026-01-22 15:53:18 +0100

    comment out hello world

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/add", (int num1, int num2) => AddNumbers(num1, num2));
 
