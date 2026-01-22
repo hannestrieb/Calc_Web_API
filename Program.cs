1625ffa4392ae8f8aa142936198069945d95ccdf
hannestrieb <triebhannes@gmail.com>
2026-01-22 15:50:32 +0100

    added html file

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/add", (int num1, int num2) => AddNumbers(num1, num2));
